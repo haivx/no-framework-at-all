c826529449cf85b3631a08e522c8176637e7d363
Xuan Hai <haivx0206@gmail.com>
2023-12-31 23:54:19 +0700

    add game

---
+++ README.md
@@ -0,0 +1,5 @@
+# Source: JavaScript Game Development Course for Beginners ~freecodecamp
+
+# Material resource:
+- https://opengameart.org
+- https://bevouliin.com/
---
+++ collisons-animation/index.js
@@ -0,0 +1,78 @@
+/** @type {HTMLCanvasElement} */
+
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+CANVAS_WIDTH = canvas.width = 700;
+CANVAS_HEIGHT = canvas.height = 500;
+let gameFrame = 0;
+let canvasPosition = canvas.getBoundingClientRect();
+
+const explosions = [];
+
+class Explosion {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.spriteWidth = 200;
+    this.spriteHeight = 179;
+    this.width = this.spriteWidth / 2;
+    this.height = this.spriteHeight / 2;
+    this.image = new Image();
+    this.image.src = "boom.png";
+    this.frame = 0;
+    this.timer = 0;
+    this.angle = Math.random() * 6.2;
+    this.sound = new Audio();
+    this.sound.src = "clickable.mp3";
+  }
+  update() {
+    if (this.frame === 0) this.sound.play();
+    this.timer++;
+    if (this.timer % 10 === 0) {
+      this.frame++;
+    }
+  }
+  draw() {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.angle);
+    ctx.drawImage(
+      this.image,
+      this.spriteWidth * this.frame,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      0 - this.width / 2,
+      0 - this.height / 2,
+      this.width,
+      this.height
+    );
+    ctx.restore();
+  }
+}
+
+window.addEventListener("click", function (e) {
+  createAnimation(e);
+});
+
+function createAnimation(e) {
+  let positionX = e.x - canvasPosition.left;
+  let positionY = e.y - canvasPosition.top;
+  explosions.push(new Explosion(positionX, positionY));
+  console.log(explosions);
+}
+
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  for (let i = 0; i < explosions.length; i++) {
+    explosions[i].update();
+    explosions[i].draw();
+    if (explosions[i].frame > 5) {
+      explosions.splice(i, 1);
+      i--;
+    }
+  }
+  requestAnimationFrame(animate);
+}
+
+animate();
